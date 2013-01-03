157c9f5e155f6d1951ac58da1824132d80a02c48
Maarten Balliauw <maarten@balliauw.be>
2013-01-03 12:49:59 +0100

    Added MvcSiteMapProvider.Core and MvcSiteMapProvider.Web projects
    Added MvcSiteMapProvider.Core and MvcSiteMapProvider.Web projects

---
+++ src/MvcSiteMapProvider/MvcSiteMapProvider.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("MvcSiteMapProvider.Web")]
+[assembly: AssemblyDescriptionAttribute("An ASP.NET SiteMapProvider implementation for the ASP.NET MVC framework.")]
